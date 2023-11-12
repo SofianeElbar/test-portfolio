908038381bc04452e787f55bd65f706ea17836a8
SofianeElbar <sofiane.elbar@hotmail.fr>
2023-11-12 19:05:03 +0100

    background

--- app.vue
+++ app.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="flex flex-col min-h-screen">
+  <div
+    class="flex flex-col min-h-screen bg-cover bg-center bg-fixed"
+    style="background-image: url('/images/background.jpg')"
+  >
     <SiteHeader />
-    <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow items-start w-full">
+    <main
+      class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow items-start w-full"
+    >
       <NuxtPage />
     </main>
     <SiteFooter />
--- pages/projets/index.vue
+++ pages/projets/index.vue
@@ -13,10 +13,10 @@ const sortedPosts = posts.value.sort((a, b) => {
 
 <template>
   <h1 class="text-3xl my-8">Divers projets</h1>
-
-  Voici une liste non exhaustive de mes projets web, qu'ils soient personnels ou
-  professionels
-
+  <p class="text-lg mb-8">
+    Voici une liste non exhaustive de mes projets web, qu'ils soient personnels
+    ou professionels
+  </p>
   <section class="grid md:grid-cols-3 mt-8 mb-8 gap-10">
     <Post :posts="posts" />
   </section>
