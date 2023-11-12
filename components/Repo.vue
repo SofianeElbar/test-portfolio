97a55b1f7851777cbd564677f3a4aaa7322a3582
SofianeElbar <sofiane.elbar@hotmail.fr>
2023-11-12 14:51:40 +0100

    repo component

---
+++ components/Repo.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({ repos: Object });
+</script>
+
+<template>
+  <div
+    v-for="github in props.repos?.viewer.repositories.nodes"
+    :key="github.id"
+    :class="props.class"
+    class="p-8 border-4 my-4 rounded-lg bg-gray-50 shadow-xl hover:bg-slate-200"
+  >
+    <a :href="github.url" target="_blank">
+      <h2 class="text-2xl text-indigo-800 font-semibold mb-2 hover:underline">
+        {{ github.name }}
+      </h2>
+    </a>
+    <p>{{ github.description }}</p>
+    <div class="mt-4">
+      <Icon name="fontisto:star" size="1.1rem" class="text-indigo-700" />
+      Stars: {{ github.stargazers.totalCount }}
+      <Icon name="system-uicons:branch" size="1.1rem" class="text-indigo-700" />
+      Forks: {{ github.forks.totalCount }}
+      <Icon name="system-uicons:eye" size="1.1rem" class="text-indigo-700" />
+      Watchers: {{ github.watchers.totalCount }}
+    </div>
+  </div>
+</template>
--- pages/github.vue
+++ pages/github.vue
@@ -1,49 +1,41 @@
 <script setup>
 const query = gql`
-{
-  viewer {
-    repositories(first: 6, orderBy:{field:CREATED_AT,direction: DESC}) {
-      totalCount
-      nodes {
-        id
-        name
-        createdAt
-        description
-        url
-        forks {
-          totalCount
-        }
-        watchers {
-          totalCount
-        }
-        stargazers {
-          totalCount
+  {
+    viewer {
+      repositories(first: 8, orderBy: { field: CREATED_AT, direction: DESC }) {
+        totalCount
+        nodes {
+          id
+          name
+          createdAt
+          description
+          url
+          forks {
+            totalCount
+          }
+          watchers {
+            totalCount
+          }
+          stargazers {
+            totalCount
+          }
         }
       }
     }
   }
-}
-`
+`;
 
-const { data } = await useAsyncQuery(query);
+const { data: repos } = await useAsyncQuery(query);
+console.log(repos);
 </script>
 
 <template>
-  <h1 class="text-3xl my-8">Projects</h1>
-  <p class="text-lg mb-8">Here are some of my projects on GitHub.</p>
-  <section class="grid grid-cols-2 gap-10">
-    <div v-for="project in data?.viewer.repositories.nodes" :key="project.id"
-      class="p-8 border-4 my-4 rounded-lg hover:bg-gray-50">
-      <a :href="project.url" target="_blank">
-        <h2 class="text-2xl text-indigo-800 font-semibold mb-2 hover:underline">{{ project.name }}</h2>
-      </a>
-      <p>{{ project.description }}</p>
-      <div class="mt-4">
-        <Icon name="fontisto:star" size="1.1rem" class="text-indigo-700" /> Stars: {{ project.stargazers.totalCount }}
-        <Icon name="system-uicons:branch" size="1.1rem" class="text-indigo-800" /> Forks: {{ project.forks.totalCount }}
-        <Icon name="system-uicons:eye" size="1.1rem" class="text-indigo-700" /> Watchers: {{
-          project.watchers.totalCount }}
-      </div>
-    </div>
+  <h1 class="text-3xl my-8">Codes sources de mes projets sur GitHub</h1>
+  <p class="text-lg mb-8">
+    Si vous désirez jeter un coup d'oeil au code source de mes projets, vous
+    êtes sur la bonne page ;)
+  </p>
+  <section class="grid grid-cols-2 mb-10 gap-10">
+    <Repo :repos="repos" />
   </section>
 </template>
